d4ecf602162fb50ea9239aba67b66e3e8cc79cc9
Mostafa2132 <12m0stafa7@gmail.com>
2024-10-03 13:18:40 +0300

    final9

--- Js/index.js
+++ Js/index.js
@@ -30,7 +30,7 @@ function DisplayAllCategories(categoryList) {
   }
   let allCatigre = document.querySelectorAll(".category");
   for (let i = 0; i < allCatigre.length; i++) {
-    if (location.pathname.includes("./index.html")) {         ////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! tmam
+    if (location.pathname.includes("/index.html")) {         ////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! tmam
       allCatigre[i].addEventListener("click", () => {
         localStorage.setItem("category", allCatigre[i].innerText);
         location.href = "/categoryProducts.html";            ////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! +  
